008b11747ef8f3dd9516000b17b0c83e06851dce
saroj <pandeysarojofficial@gmail.com>
2021-08-08 20:21:26 +0545

    first commit

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,14 @@
+class HomesController < ApplicationController
+  def index
+    #------------dynamic data for chart can be passed from here like below------- ### 
+    # @data_keys = [
+    #   'January',
+    #   'February',
+    #   'March',
+    #   'April',
+    #   'May',
+    #   'June',
+    # ]
+    # @data_values = [0, 10, 5, 2, 20, 30, 45]
+  end
+end
